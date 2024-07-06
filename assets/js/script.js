84b54c010ad2aff549713a2827f6a2539bc957a5
Omprakash Pal <omprakshpal739@gmail.com>
2024-07-07 00:28:20 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+// Mobile menu toggler
+const toggleButton = document.getElementById('nav-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+})
+
